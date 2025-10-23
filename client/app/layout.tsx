7f087f3ab0db5619c5b0e78942cc375e968dc789
Ali_Shair <alishair7071@gmail.com>
2025-10-23 19:21:55 +0500

    before mids

---
+++ client/app/layout.tsx
@@ -0,0 +1,36 @@
+import "./globals.css";
+import { Poppins } from "next/font/google";
+import { Josefin_Sans } from "next/font/google";
+import { ThemeProvider } from "./utils/theme-provider";
+
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-Poppins",
+});
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-Josefin",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${poppins.variable} ${josefin.variable} !bg-white dark:bg-gradient-to-b dark:from-gray-900 dark:to-black duration-300 bg-no-repeat`}
+      >
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
